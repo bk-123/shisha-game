b2292bbda69231ca768ce8ddbfb0c1195455e2ca
bk-123 <47869607+bk-123@users.noreply.github.com>
2019-02-21 22:25:23 +0000

    shisha

---
+++ Assets/Scripts/Home Scene Scripts/HomeToAchievements.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeToAchievements : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+
+        SceneManager.LoadScene("Achievements");
+
+    }
+
+}
