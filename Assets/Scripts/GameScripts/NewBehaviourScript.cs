46148ef02deb4dc151409d41578191b32b060fb4
bk-123 <47869607+bk-123@users.noreply.github.com>
2019-02-22 19:03:23 +0000

    shisha

--- Assets/Scripts/GameScripts/NewBehaviourScript.cs
+++ Assets/Scripts/GameScripts/NewBehaviourScript.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    Vector3 startPos;
+
+    private Vector3 offset;
+    private Vector3 oldPos;
+
+    void OnMouseDown()
+    {
+
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+
+    void OnMouseUp()
+    {
+        transform.position = oldPos;
+    }
+
 }
