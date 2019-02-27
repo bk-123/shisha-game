31a481b46be3670001f0591f5546ec6aac4bda49
bk-123 <47869607+bk-123@users.noreply.github.com>
2019-02-27 16:29:18 +0000

    new

---
+++ Assets/Multiply.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiply : MonoBehaviour {
+
+    public GameObject fruit;
+    GameObject fruitClone;
+
+    private void Start()
+    {
+        fruitClone = Instantiate(fruit.transform.position) as GameObject;
+    }
+
+    private GameObject Instantiate(object identity)
+    {
+        throw new NotImplementedException();
+    }
+}
